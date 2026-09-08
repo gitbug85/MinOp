3b2f179d0d39d8dc2fefeb5dd626427b41c4c1b5
gitbug85 <140214265+gitbug85@users.noreply.github.com>
2026-09-08 09:32:09 -0600

    More work on the Nim lexer

--- src/lexer.nim
+++ src/lexer.nim
@@ -15,6 +15,25 @@ proc print_segs(segs: seq[Segment]) =
 proc append_to_seg(seg: var Segment, other: var Segment) =
     seg.val &= other.val
 
-proc lex(path: string): seq[Segment] =
-    return @[]
+proc lex(filename: string): seq[Segment] =
+    var fragments: seq[Segment] = @[]
+
+    for line in lines(filename):
+        var new_line = line
+        let commentIdx = new_line.find('#')
+        if commentIdx != -1:
+            new_line = new_line[0 ..< commentIdx]
+        var count = 0
+        while count < new_line.len and new_line[count] == ' ':
+            inc(count)
+        new_line = new_line[count .. ^1]
+        let tabs = count div 4
+        let leftover = count mod 4
+        
+        for _ in 0 .. tabs-1:
+            fragments.add(Segment(val: "  ", col: 0, ln: 0))
+
+        echo line
+
+    return fragments
 
